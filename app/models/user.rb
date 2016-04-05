fcfa997dfb74d8eed6fd35a4b3980d4c02b98584
J-Henry <JH@JH.local>
2016-04-05 15:31:38 -0400

    Validate uniqueness of user email and username #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :things
   has_many :votes
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
